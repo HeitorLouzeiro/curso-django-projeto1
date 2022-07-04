00a3cdcd1598bf25eaf7a6e804db08e168b06091
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-07-04 12:30:59 -0300

    adicionado models no admin,configurado imagens.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Category, Recipe
+
+
 # Register your models here.
+class CategoryaAdmin(admin.ModelAdmin):
+    ...
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryaAdmin)
+admin.site.register(Recipe, RecipeAdmin)
