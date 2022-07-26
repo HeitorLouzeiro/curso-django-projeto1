7e1765aad9694439971db0483608b1ad053d4053
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-07-26 12:36:39 -0300

    instalando debug toolbar e resolvendo problemas de colsulta SQL

--- project/settings.py
+++ project/settings.py
@@ -43,10 +43,14 @@ INSTALLED_APPS = [
     # A gente tem apps também
     'recipes',
     'authors',
+
+    # Debug Toolbar
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,3 +150,10 @@ MESSAGE_TAGS = {
     constants.SUCCESS: 'message-success',
     constants.WARNING: 'message-warning',
 }
+
+# django debug toolbar
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+    # ...
+]
