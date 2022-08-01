d4b07427ddd15efe2dc4f74e0981090ab7ab41a1
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-08-01 11:33:49 -0300

    Mixins, Generic Views e Pagination no Django Rest Framework

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from tag.models import Tag
@@ -83,27 +85,37 @@ from ..serializers import RecipeSerializer, TagSerializer
 #     return Response(serializer.data)
 
 # CBV Django Framework Rest Framework
-class RecipeAPIv2List(APIView):
-    def get(self, request):
-        recipes = Recipe.objects.get_published()[:10]
-        serializer = RecipeSerializer(
-            instance=recipes,
-            many=True,
-            context={'request': request},
-        )
-        return Response(serializer.data)
+# class RecipeAPIv2List(APIView):
+#     def get(self, request):
+#         recipes = Recipe.objects.get_published()[:10]
+#         serializer = RecipeSerializer(
+#             instance=recipes,
+#             many=True,
+#             context={'request': request},
+#         )
+#         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = RecipeSerializer(
-            data=request.data,
-            context={'request': request},
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-        )
+#     def post(self, request):
+#         serializer = RecipeSerializer(
+#             data=request.data,
+#             context={'request': request},
+#         )
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(
+#             serializer.data,
+#             status=status.HTTP_201_CREATED
+#         )
+
+
+class RecipeAPIv2Pagination(PageNumberPagination):
+    page_size = 2
+
+
+class RecipeAPIv2List(ListCreateAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
 
 
 class RecipeAPIv2Detail(APIView):
