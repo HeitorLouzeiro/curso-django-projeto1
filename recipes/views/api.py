da6c8dea56a4518650898785e8512f0aca1834a9
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-08-01 12:26:59 -0300

    RetrieveUpdateDestroyAPIView

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status
+# from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 from tag.models import Tag
 
 from ..models import Recipe
@@ -105,9 +106,47 @@ from ..serializers import RecipeSerializer, TagSerializer
 #         return Response(
 #             serializer.data,
 #             status=status.HTTP_201_CREATED
+
+# class RecipeAPIv2Detail(APIView):
+#     def get_recipe(self, pk):
+#         recipe = get_object_or_404(
+#             Recipe.objects.get_published(),
+#             pk=pk
+#         )
+#         return recipe
+
+#     def get(self, request, pk):
+#         recipe = self.get_recipe(pk)
+#         serializer = RecipeSerializer(
+#             instance=recipe,
+#             many=False,
+#             context={'request': request},
+#         )
+#         return Response(serializer.data)
+
+#     def patch(self, request, pk):
+#         recipe = self.get_recipe(pk)
+#         serializer = RecipeSerializer(
+#             instance=recipe,
+#             data=request.data,
+#             many=False,
+#             context={'request': request},
+#             partial=True,
+#         )
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(
+#             serializer.data,
 #         )
 
+#     def delete(self, request, pk):
+#         recipe = self.get_recipe(pk)
+#         recipe.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
+#         )
+
+# Gereneric DRF
 class RecipeAPIv2Pagination(PageNumberPagination):
     page_size = 2
 
@@ -118,25 +157,15 @@ class RecipeAPIv2List(ListCreateAPIView):
     pagination_class = RecipeAPIv2Pagination
 
 
-class RecipeAPIv2Detail(APIView):
-    def get_recipe(self, pk):
-        recipe = get_object_or_404(
-            Recipe.objects.get_published(),
-            pk=pk
-        )
-        return recipe
+class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
 
-    def get(self, request, pk):
-        recipe = self.get_recipe(pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            many=False,
-            context={'request': request},
-        )
-        return Response(serializer.data)
+    def patch(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
 
-    def patch(self, request, pk):
-        recipe = self.get_recipe(pk)
+        recipe = self.get_queryset().filter(pk=pk).first()
         serializer = RecipeSerializer(
             instance=recipe,
             data=request.data,
@@ -149,11 +178,6 @@ class RecipeAPIv2Detail(APIView):
         return Response(
             serializer.data,
         )
-
-    def delete(self, request, pk):
-        recipe = self.get_recipe(pk)
-        recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
