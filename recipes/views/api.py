6e9a82035a28ec0a7d7b2564353d73cf00f7189f
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-08-01 12:44:51 -0300

    Usando viewsets

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_object_or_404
 # from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.generics import (ListCreateAPIView,
-                                     RetrieveUpdateDestroyAPIView)
+# from rest_framework.generics import (ListCreateAPIView,RetrieveUpdateDestroyAPIView)
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 # from rest_framework.views import APIView
 from tag.models import Tag
+from rest_framework.viewsets import ModelViewSet
 
 from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
@@ -147,17 +147,44 @@ from ..serializers import RecipeSerializer, TagSerializer
 #         )
 
 # Gereneric DRF
+# class RecipeAPIv2Pagination(PageNumberPagination):
+#     page_size = 2
+
+
+# class RecipeAPIv2List(ListCreateAPIView):
+#     queryset = Recipe.objects.get_published()
+#     serializer_class = RecipeSerializer
+#     pagination_class = RecipeAPIv2Pagination
+
+
+# class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
+#     queryset = Recipe.objects.get_published()
+#     serializer_class = RecipeSerializer
+#     pagination_class = RecipeAPIv2Pagination
+
+#     def patch(self, request, *args, **kwargs):
+#         pk = kwargs.get('pk')
+
+#         recipe = self.get_queryset().filter(pk=pk).first()
+#         serializer = RecipeSerializer(
+#             instance=recipe,
+#             data=request.data,
+#             many=False,
+#             context={'request': request},
+#             partial=True,
+#         )
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(
+#             serializer.data,
+#         )
+
+# ViewSet DRF
 class RecipeAPIv2Pagination(PageNumberPagination):
     page_size = 2
 
 
-class RecipeAPIv2List(ListCreateAPIView):
-    queryset = Recipe.objects.get_published()
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeAPIv2Pagination
-
-
-class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
+class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
